0eacd1b9cac5f14368fd6c77a36bd4cc0cf64c98
448102337@qq.com <!QAZ1qaz1qaz>
2015-05-05 21:17:01 +0800

    TTTT

--- corebeau/src/com/zifei/corebeau/my/task/ProfileImageTask.java
+++ corebeau/src/com/zifei/corebeau/my/task/ProfileImageTask.java
@@ -178,23 +178,35 @@ public class ProfileImageTask {
 	@SuppressLint("NewApi")
 	public Bitmap compressImage(Bitmap image) {
 		Log.i("image compressImage before", image.getByteCount() + " ");
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
-		if (baos.toByteArray().length / 1024 <= CommonConfig.UPLOAD_IMAGE_QUALITY) {
-			ByteArrayInputStream isBm = new ByteArrayInputStream(
-					baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
-			Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
-			return bitmap;
-		}
-		int options = 100;
-		while (baos.toByteArray().length / 1024 > CommonConfig.UPLOAD_IMAGE_QUALITY) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
-			baos.reset();// 重置baos即清空baos
-			image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
-			options -= 10;// 每次都减少10
-		}
-		ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
-		Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
-		return bitmap;
+//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//		image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+//		if (baos.toByteArray().length / 1024 <= CommonConfig.UPLOAD_IMAGE_QUALITY) {
+//			ByteArrayInputStream isBm = new ByteArrayInputStream(
+//					baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
+//			Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
+//			return bitmap;
+//		}
+//		int options = 100;
+//		while (baos.toByteArray().length / 1024 > CommonConfig.UPLOAD_IMAGE_QUALITY) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
+//			baos.reset();// 重置baos即清空baos
+//			image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
+//			options -= 10;// 每次都减少10
+//		}
+//		ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
+//		Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
+//		return bitmap;
+		
+		 ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+	        int options = 100;  
+	        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);  
+	        while (baos.toByteArray().length / 1024 >  CommonConfig.UPLOAD_IMAGE_QUALITY) {   
+	            baos.reset();  
+	            options -= 10;  
+	            image.compress(Bitmap.CompressFormat.JPEG, options, baos);  
+	        }  
+	        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());  
+	        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);  
+	        return bitmap;  
 	}
 
 	
